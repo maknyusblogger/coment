81c13add1dd203514c2bd60fc0c4408b2b77945e
maknyusblogger <jegerdotkom@gmail.com>
2015-11-27 02:51:12 +0700

    Update and rename coment.js to balas.js

--- balas.js
+++ balas.js
@@ -1,9 +1,10 @@
- (function() {
+// <![CDATA[
+    (function() {
       var items = <data:post.commentJso/>;
       var msgs = <data:post.commentMsgs/>;
       var config = <data:post.commentConfig/>;
 
-// <![CDATA[
+
       var cursor = null;
       if (items && items.length > 0) {
         cursor = parseInt(items[items.length - 1].timestamp) + 1;
